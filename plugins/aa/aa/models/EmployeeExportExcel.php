6e766e71d16f1c685ca6778a23e3be1b3f34aec0
jaber <jaber@acornassoicated.org>
2026-08-12 13:08:00 +0300

    update

--- plugins/aa/aa/models/EmployeeExportExcel.php
+++ plugins/aa/aa/models/EmployeeExportExcel.php
@@ -4,11 +4,33 @@
 
 class EmployeeExportExcel extends \Backend\Models\ExportModel
 {
-    public function exportData($columns, $sessionKey = null)
+
+
+        public function exportData($columns, $sessionKey = null)
     {
-        foreach (Employee::cursor() as $record) {
-            $record->addVisible($columns);
-            yield $record->toArray();
+        // جلب البيانات مع العلاقات
+        $cars = Employee::with(['center', 'salare'])->cursor();
+        
+        foreach ($cars as $record) {
+            $exportData = $record->toArray();
+            
+            // إضافة العلاقات مع تحويل آمن
+            $exportData['center'] = $record->center ? $record->center->name : 'لا يوجد';
+            $exportData['salare'] = $record->salare ? $record->salare->FullQualityName : 'لا يوجد';
+
+            $exportData['gender'] = $record->gender ?  trans('aa.aa::lang.model.employee.' . $record->gender) : 'لا يوجد';
+            $exportData['nationality'] = $record->nationality ?  trans('aa.aa::lang.model.employee.' . $record->nationality) : 'لا يوجد';
+
+            
+
+            
+            // تصفية الأعمدة
+            $finalData = [];
+            foreach ($columns as $column) {
+                $finalData[$column] = $exportData[$column];
+            }
+            
+            yield $finalData;
         }
     }
 }
